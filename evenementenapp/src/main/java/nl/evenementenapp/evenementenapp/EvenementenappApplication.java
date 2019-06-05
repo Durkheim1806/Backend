61ca2a2baaef36b12dbe47b2c2905f27054075dc
Durkheim1806 <derkbreslau@hotmail.com>
2019-06-05 13:02:47 +0200

    spring initialisatie

---
+++ evenementenapp/src/main/java/nl/evenementenapp/evenementenapp/EvenementenappApplication.java
@@ -0,0 +1,13 @@
+package nl.evenementenapp.evenementenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenementenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenementenappApplication.class, args);
+	}
+
+}
