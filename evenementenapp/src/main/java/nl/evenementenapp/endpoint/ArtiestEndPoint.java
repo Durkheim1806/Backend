c7ab76fe9b00f93e72fa77079b91da6158c75f00
Durkheim1806 <derkbreslau@hotmail.com>
2019-06-06 11:10:43 +0200

    laatste versie2

--- evenementenapp/src/main/java/nl/evenementenapp/endpoint/ArtiestEndPoint.java
+++ evenementenapp/src/main/java/nl/evenementenapp/endpoint/ArtiestEndPoint.java
@@ -1,5 +1,10 @@
 package nl.evenementenapp.endpoint;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
+@RestController
+@Request
 public class ArtiestEndPoint {
 
 }
